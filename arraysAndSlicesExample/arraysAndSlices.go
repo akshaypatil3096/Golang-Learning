338276912627af8b8fc8bffbb88b75733896a961
akshay <akshay.patil3096@gmail.com>
2022-05-25 13:24:18 +0530

    arrays and slices example

---
+++ arraysAndSlicesExample/arraysAndSlices.go
@@ -0,0 +1,26 @@
+package arraysandslicesexample
+
+import "fmt"
+
+func ArrayExample() {
+	arr1 := [4]int{1, 2, 3, 4}
+	fmt.Println("arr1 is ----> ", arr1)
+	arr1[3] = 40
+	fmt.Println("arr1 after chaning  is ----> ", arr1)
+	arr2 := arr1
+	fmt.Println("arr2 --> ", arr2, " length is --> ", len(arr2), "capacity is --> ", cap(arr2))
+	arr3 := arr2[1:3]
+	fmt.Println("arr3 --> ", arr3, " length is --> ", len(arr3), "capacity is --> ", cap(arr3))
+	//below line will give panic: runtime error: index out of range [4] with length 2
+	//arr3[4] = 32
+	arr4 := arr1
+	fmt.Println(arr4)
+}
+
+func SliceExample() {
+	arr1 := []int{}
+	fmt.Println("In slice arr1 --> ", arr1, " length is --> ", len(arr1), "capacity is --> ", cap(arr1))
+	arr1 = append(arr1, 12)
+	fmt.Println("In slice, After append arr1 --> ", arr1, " length is --> ", len(arr1), "capacity is --> ", cap(arr1))
+
+}
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	hangmangame "goAssignments/hangmanGame"
-	pointerslearning "goAssignments/pointersLearning"
+	arraysandslicesexample "goAssignments/arraysAndSlicesExample"
+	_ "goAssignments/hangmanGame"
+	_ "goAssignments/pointersLearning"
 )
 
 func main() {
-	pointerslearning.PointersBasics()
-	hangmangame.Hangman()
+	//pointerslearning.PointersBasics()
+	//hangmangame.Hangman()
+	arraysandslicesexample.ArrayExample()
+	arraysandslicesexample.SliceExample()
 }
